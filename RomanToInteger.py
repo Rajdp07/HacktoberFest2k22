d5d3615877e033b83bdfc151be83766862597c20
VaibhavDubeySSE <vaibhav.dubey@lntinfotech.com>
2022-10-15 13:43:20 +0530

    added a python script to convert roman to Integer

---
+++ RomanToInteger.py
@@ -0,0 +1,13 @@
+s='MCMXCIV' #this can be any roman string 
+
+roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500 , 'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+i=0
+num = 0
+while(i<len(s)):
+    if(i<len(s)-1 and s[i:i+2] in roman):
+        num+=roman[s[i:i+2]]
+        i+=2
+    else:
+        num+=roman[s[i]]
+        i+=1
+print(num)
